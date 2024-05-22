08cfd1ace326b62c920275b2dac119d3f0215faf
Alex <xrlogic1@gmail.com>
2024-05-22 11:10:41 +0400

    Initial commit

---
+++ ArraysAndStrings/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+// bool, byte, sbyte, short, char, int, float, double, long, decimal
+// string? input = Console.ReadLine();
+//
+// if (input != null)
+// {
+//     var result = double.Parse(input);
+//     
+//     Console.WriteLine(result * 2);
+// }
+
+
+// Arrays
+// int[] arr = { 10, 20, 30 };
+// int[] arr1 = new int[3] { 10, 20, 30 };
+// int[] arr2 = new int[6];
+// int[] arr3 = [10, 20, 30]; // NET 8 only
+//
+//
+// // var random = new Random();
+//
+// for (int i = 0; i < arr2.Length; i++)
+// {
+//     //arr2[i] = random.Next(10, 99);
+//     arr2[i] = Random.Shared.Next(10, 99);
+// }
+
+// Multidimensional Arrays
+
+// int[,] mdArr = new int[4,5]
+// { 
+//   // 0   1   2   3   4
+//     {10, 20, 30, 40, 50}, // 0
+//     {11, 24, 38, 44, 59}, // 1
+//     {12, 25, 37, 43, 52}, // 2
+//     {13, 26, 36, 42, 51}, // 3
+// };
+//
+//
+// // Console.WriteLine(mdArr[1, 1]);
+// Console.WriteLine($"Length = {mdArr.Length}");
+// Console.WriteLine($"Rank = {mdArr.Rank}");
+//
+// for (int i = 0; i <= mdArr.GetUpperBound(0); i++)
+// {
+//     Console.Write("{ ");
+//     for (int j = 0; j <= mdArr.GetUpperBound(1); j++)
+//     {
+//         Console.Write($"{mdArr[i, j]} ");
+//     }
+//
+//     Console.WriteLine("}");
+// }
+
+// Jagged Arrays
+// int[][] jagged = new int[3][];
+//
+// jagged[0] = new int[4];
+// jagged[1] = new int[3];
+// jagged[2] = new int[2];
+//
+// for (int i = 0; i < jagged.Length; i++)
+// {
+//     for (int j = 0; j < jagged[i].Length; j++)
+//     {
+//         jagged[i][j] = Random.Shared.Next();
+//     }
+// }
+//
+// for (int i = 0; i < jagged.Length; i++)
+// {
+//     Console.Write("{ ");
+//     for (int j = 0; j < jagged[i].Length; j++)
+//     {
+//         Console.Write($"{jagged[i][j]} ");
+//     }
+//     Console.WriteLine("}");
+// }
+
+
+// Lists
+// List<int> values = new List<int>();
+//
+// values.Add(10);
+// values.Add(20);
+// values.Add(30);
+//
+// foreach (var value in values)
+// {
+//     Console.WriteLine(value);
+// }
+
+// int?[]? arr = new int?[] { null, 42, null, 13 };
+//
+// Console.WriteLine(arr[0]);
+//
+// Console.ReadLine();
+
+
+// null conditional
+// string? text = null;
+
+// var result = text?.ToUpper(); // null conditional
+
+// if (text != null)
+// {
+//     var result = text.ToUpper();
+// }
+
+
+// null coalescenting operator
+
+// string? fathersName = null;
+//
+// var res = fathersName ?? "Fathers name not specified";
+
+
+// string text = "Hello C++";
+//
+// var result = text.Replace("C++", "C#");
+//
+// Console.WriteLine(result[^2..]);
+
+string?[] values = new string[3];
+
+Console.WriteLine(values[0] == null);
+Console.WriteLine(values[1] == null);
+Console.WriteLine(values[2] == null);
+
+string val = default;
+int? intValue = default;
+
+Console.WriteLine($"val = {val}");
+Console.WriteLine($"intValue = {intValue}");
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
---
+++ NET/ConsoleApp1/Program.cs
@@ -0,0 +1,146 @@
+﻿// CLR - Common Language Runtime
+// CLS - Common Language Specification
+// CTS - Common Type System
+// BCL/FCL - Base Class Library / Framework Class Libaray
+
+// Top level statements
+
+// 1b
+// bool
+// byte
+// 2b
+
+
+// 1b
+bool _b = false;
+byte _bt = 80;
+
+// 2b
+char c = '~';
+ushort s = 1000;
+
+// 4b
+UInt32 value = 42U;
+float f = 12.0f;
+
+// 8b
+
+var l = 10L;
+ulong ul = 10UL;
+double d = 12.0;
+
+// 16b
+
+decimal d1 = 0.1m;
+decimal d2 = 0.2m;
+
+Change(d1, d2);
+
+Console.WriteLine(d1 + d2);
+
+void Change(decimal a, decimal b)
+{
+    a *= 2;
+    b *= 2;
+}
+
+// NullReference handling 1
+// string? lang = Console.ReadLine();
+//
+// if (lang != null)
+// {
+//     Console.WriteLine(lang.ToUpper());
+// }
+
+// NullReference handling 2
+
+// string lang = Console.ReadLine()!; // <-- not null
+//
+// Console.WriteLine(lang.ToUpper());
+
+
+////////////////////////////////////////
+
+// Parsing with Parse method
+// string a = Console.ReadLine()!;
+// string b = Console.ReadLine()!;
+//
+// int val1 = int.Parse(a);
+// int val2 = int.Parse(b);
+//
+// Console.WriteLine(val1 + val2);
+
+// Parsing with TryParse method
+// string a = Console.ReadLine()!;
+// string b = Console.ReadLine()!;
+//
+// if (int.TryParse(a, out int val1) && int.TryParse(b, out int val2))
+// {
+//     Console.WriteLine(val1 + val2);
+// }
+
+/////////// if vs switch case 
+// int a = int.Parse(Console.ReadLine()!);
+// string op = Console.ReadLine()!;
+// int b = int.Parse(Console.ReadLine()!);
+
+// if (op == "+")
+// {
+//     Console.WriteLine(a + b);
+// }
+// else if (op == "-")
+// {
+//     Console.WriteLine(a - b);
+// }
+// else
+// {
+//     Console.WriteLine("Incorrect operation");
+// }
+
+// switch (op)
+// {
+//     case "plus":
+//         Console.Write("a + b = ");
+//         Console.WriteLine(a + b);
+//         break;
+//     case "+":
+//         goto case "plus";
+//     case "-":
+//         Console.WriteLine(a - b);
+//         break;
+// }
+/////////// if vs switch case [END]
+
+/////////// loops
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(i);
+}
+
+int it = 0;
+
+while (true)
+{
+    if (it++ == 3)
+    {
+        break;
+    }
+}
+
+string message = "Hello my friend";
+
+// for (int i = 0; i < message.Length; i++)
+// {
+//     Console.WriteLine(message[i]);
+// }
+
+
+foreach (var symbol in message)
+{
+    Console.WriteLine(symbol);
+}
+
+
+
+Console.Read();
